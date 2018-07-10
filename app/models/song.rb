3c9b2b9ca96b3adae4c065a39ea82c85966d7f64
Zac Willmington <zac.willmington@gmail.com>
2018-07-10 15:44:54 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,6 +2,6 @@ class Song < ActiveRecord::Base
   belongs_to :artist
 
   def song_artist_link
-      self.title + "-" + self.artist.name
+      self.title.capitalize + "-" + self.artist.name.capitalize
   end
 end
