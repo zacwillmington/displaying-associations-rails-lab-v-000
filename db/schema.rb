b0bbc3f0640a29e64ac9e78f7cad38848d363f47
Zac Willmington <zac.willmington@gmail.com>
2018-07-10 15:19:32 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180710150629) do
+ActiveRecord::Schema.define(version: 20180710151820) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -23,7 +23,7 @@ ActiveRecord::Schema.define(version: 20180710150629) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "artists_id"
+    t.integer  "artist_id"
   end
 
 end
