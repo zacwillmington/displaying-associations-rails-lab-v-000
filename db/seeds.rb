6c7904f03eff71875ac0254d87f2975280d3463e
Zac Willmington <zac.willmington@gmail.com>
2018-07-10 15:50:53 +0000

    automatically backed up by learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Artist.create(name: "fushoush")
+Song.create(title: "songogso", artist_id: 1)
